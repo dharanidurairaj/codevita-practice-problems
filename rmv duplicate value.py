95c05d5ac81fafa075b7625faadef2e26d36ea98
dharanidurairaj <52208308+dharanidurairaj@users.noreply.github.com>
2020-02-25 16:32:57 +0530

    Update rmv duplicate value.py

--- rmv duplicate value.py
+++ rmv duplicate value.py
@@ -14,3 +14,18 @@ for j in l:
    f=l.count(j)
    if f==1:
        print(list(j))
+
+#same program:         
+         
+         
+n=int(input())
+l=[]
+l1=[]
+for i in range(0,n):
+    b=input()
+    l.append(b)
+for i in l:
+    if(1==l.count(i)):
+        l1.append(i)
+l=l1
+print(l)
