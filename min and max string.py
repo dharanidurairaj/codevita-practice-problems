275e075643959dc2e0eb84f2ec77cd220643a90b
dharanidurairaj <52208308+dharanidurairaj@users.noreply.github.com>
2020-02-25 14:50:06 +0530

    Create min and max string.py

---
+++ min and max string.py
@@ -0,0 +1,45 @@
+i/p:programming in python by
+o/p:programmnig
+by
+in
+
+
+a=input().strip(".").split(" ")
+l=[]
+for i in a:
+    b=len(i)
+    l.append(b)
+c=max(l)
+d=min(l)
+for j in a:
+    if(c==len(j)):
+        print(j)
+    if(d==len(j)):
+        print(j)
+        
+i/p:programming in python by
+o/p:
+programming
+in
+        
+        
+        
+a=list(map(str,input().strip('.').split(" ")))
+l=[]
+k=[]
+h=[]
+for i in a:
+    d=len(i)
+    l.append(d)
+f=max(l)
+d=min(l)
+for j in a:
+    if len(j)==f:
+        k.append(j)
+    elif len(j)==d:
+        h.append(j)
+print(k[0])
+print(h[0])
+
+
+    
