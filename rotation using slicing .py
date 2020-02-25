ab0ee6e05d7057611ac545a91dcc4861ffecf943
dharanidurairaj <52208308+dharanidurairaj@users.noreply.github.com>
2020-02-25 15:39:09 +0530

    Update rotation using slicing .py

--- rotation using slicing .py
+++ rotation using slicing .py
@@ -12,3 +12,15 @@ c=int(input())
 d=b[0:len(b)-c]
 e=b[len(b)-c:]
 print(e+d)
+
+########same without slicing
+
+a=int(input())
+b=list(map(str,input().split(" ")))
+c=int(input())
+l=[]
+for i in range(len(b)-c,len(b)):
+   l.append(b[i])
+for j in range(0,len(b)-c):
+   l.append(b[j])
+print(l)
