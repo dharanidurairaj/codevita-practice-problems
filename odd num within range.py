cf7871d39b2629c95f52a28e8900bfce57d76b86
dharanidurairaj <52208308+dharanidurairaj@users.noreply.github.com>
2020-02-26 11:01:24 +0530

    Create odd num within range.py

---
+++ odd num within range.py
@@ -0,0 +1,28 @@
+i/p:3
+o/p:1
+3
+
+
+
+
+a=int(input())
+for i in range(0,a+1):
+    if(i%2!=0):
+        print(i)
+    
+    
+i/p:3
+o/p:1
+3
+5
+
+
+
+def odd(i,n):
+    if(i<n):
+        print(2*i+1)
+        i=i+1
+        odd(i,n)
+n=int(input())
+odd(0,n)
+    
