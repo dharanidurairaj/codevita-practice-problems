c6a40551067dde5ca6cf6fe4cd6956a459c4ebab
dharanidurairaj <52208308+dharanidurairaj@users.noreply.github.com>
2020-02-27 10:04:50 +0530

    Update amstrong number.py

--- amstrong number.py
+++ amstrong number.py
@@ -17,3 +17,47 @@ for num in range(lower, upper + 1):
 
    if num == sum:
        print(num)
+      -----------------------------------------------------------------------------------------------------------------------------------------
+
+      
+      
+      
+      a = int(input())
+b = int(input())
+for i in range(a, b + 1):
+    d = 0
+    temp = i
+    while temp > 0:
+        temp //= 10
+        d+=1
+    sum = 0
+    temp = i
+    while temp > 0:
+        r = temp % 10
+        sum += r ** d
+        temp //= 10
+    if i == sum:
+        print(i,end=" ")
+        
+        --------------------------------------------------------------------------------------------------
+        
+        
+        
+        
+import math
+a = int(input())
+b = int(input())
+for i in range(a, b + 1):
+    d = int(math.log10(i)+1)
+    temp = i
+    sum = 0
+    temp = i
+    while temp > 0:
+        r = temp % 10
+        sum += r ** d
+        temp //= 10
+    if i == sum:
+        print(i,end=" ")
+        
+        
+        -------------------------------------------------------------------------------------------------------
